3624ee2678d4f946686cb3f1f985af1053d2ad72
Suraj vishwakarma <90332766+vishwakarmasuraj@users.noreply.github.com>
2021-10-20 15:53:29 +0530

    at 15: 52 pm
    Tables and Schema for send request, accept, decline etc.

---
+++ models/friendsRequest.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const friendSchema = new Schema({
+    requestFrom: { type: SchemaTypes.ObjectId, ref: 'Users' },
+    requestTo: { type: SchemaTypes.ObjectId, ref: 'Users' },
+    status: {
+        type: String,
+        required: true,
+        enum: ['Open', 'Close', 'Decline', 'Accept']
+    },
+    message: {
+        type: String
+    }
+
+}, { timestamps: true })
+
+module.exports = mongoose.model('FriendRequest', friendSchema)
