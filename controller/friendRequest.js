da85469398045563d7c62329c901b10fbae092b3
Suraj vishwakarma <90332766+vishwakarmasuraj@users.noreply.github.com>
2021-10-21 10:48:42 +0530

    at 10 : 48 am
    just try to resolve.

--- controller/friendRequest.js
+++ controller/friendRequest.js
@@ -4,9 +4,11 @@ const FriendRequest = require('../models/friendsRequest')
 
 const requestForUser = async (req, res) => {
     try {
-
+        const id = req.params.id
+        const result = await FriendRequest.find({ id: id })
+        res.status(200).json({ msg: 'found' })
     } catch (error) {
-
+        console.log(error)
     }
 }
 
