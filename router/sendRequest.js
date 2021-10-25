5a034e0de353a78bf524d3c79b166907074b2158
Suraj vishwakarma <90332766+vishwakarmasuraj@users.noreply.github.com>
2021-10-25 16:57:27 +0530

    at 16 : 57 pm
    just start now

--- router/sendRequest.js
+++ router/sendRequest.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 
-// const authToken = require('../middleware/authToken')
+const authToken = require('../middleware/authToken')
 const sendReqController = require('../controller/sendFriendRequest')
 
 
-router.post('/sendReq', sendReqController.requestSend)
+router.post('/sendReq', authToken.verifyToken, sendReqController.requestSend)
 
-router.get('/requestList', sendReqController.requestedList)
+router.get('/requestList/:id', authToken.requestedList, sendReqController.requestedList)
 
 module.exports = router
