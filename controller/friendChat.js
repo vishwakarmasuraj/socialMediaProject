bc172c05dbe31eb0aecf705c482fddb3bb291459
Suraj vishwakarma <90332766+vishwakarmasuraj@users.noreply.github.com>
2021-10-29 12:51:14 +0530

    at 12 : 50 pm
    just working on multer to upload file.

--- controller/friendChat.js
+++ controller/friendChat.js
@@ -1,5 +1,6 @@
 const FriendChat = require('../models/friendChat')
 const multer = require('multer')
+const constants = require('../constant/allConstants')
 
 const userChatAndSendFile = async (req, res) => {
     try {
@@ -9,6 +10,36 @@ const userChatAndSendFile = async (req, res) => {
             toUserId: req.body.toUserId,
             message: req.body.message,
             url: req.body.url
+        })
+        const storage = multer.diskStorage({
+            destination: function (req, file, cb) {
+                if (
+                    file.mimetype == 'txt' ||
+                    file.mimetype == 'FLAC' ||
+                    file.mimetype == 'MP3' ||
+                    file.mimetype == ' WAV' ||
+                    file.mimetype == 'MP4' ||
+                    file.mimetype == 'WMA' ||
+                    file.mimetype ||
+                    'AAC'
+                ) {
+                    cb(null, true)
+                } else {
+                    cb(null, false)
+                    return cb(new Error(constants.AUDIO_FORMAT_ERR))
+                }
+                cb(null, __dirname + './../uploads')
+            },
+            filename: function (req, file, cb) {
+                cb(null, file.originalname)
+            },
+        })
+
+        const upload = multer({
+            storage: storage,
+            onFileUploadStart: function (file) {
+                console.log(file.originalname + ' is starting ...')
+            },
         })
         await result.save()
         res.status(200).json({ message: 'Chating start', result })
